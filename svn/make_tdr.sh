0e8e81bd6dc3480286fc531eed8cdea824d8f0ea
SangEun Lee <d4space@gmail.com>
2018-10-10 15:55:30 +0900

    hahahah

--- svn/make_tdr.sh
+++ svn/make_tdr.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+myAN="AN-18-227"
 eval `./tdr runtime -sh`
 #tdr  --style=an b AN-13-023
 #tdr --draft --style=an b SMP-12-021
-tdr --draft --style=an b AN-16-182
+tdr --draft --style=an b $myAN
 #tdr --draft --style=an b AN-13-023
 #open tmp/SMP-12-021_temp.pdf&
-open tmp/AN-16-182_temp.pdf&
+open tmp/${myAN}_temp.pdf&
