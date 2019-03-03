1a9541b37043abfc8c10f2747c288b3c260533a3
babo Haha <yourmailID@babo.com>
2019-03-03 17:41:49 +0900

    xrdcp

---
+++ DataHandle/xrdcp.sh
@@ -0,0 +1,7 @@
+
+#From https://twiki.cern.ch/twiki/bin/view/CMSPublic/WorkBookXrootdService
+
+# You may need the voms-proxy-init --voms cms
+
+#xrdfs cms-xrd-global.cern.ch locate /store/mc/RunIISummer16MiniAODv3/DYJetsToTauTau_ForcedMuEleDecay_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_ext1/MINIAODSIM/PUMoriond17_94X_mcRun2_asymptotic_v3_ext1-v2/80000/FE78F098-0631-E911-9357-0CC47A4D769A.root
+xrdcp root://cmsxrootd.fnal.gov//store/mc/RunIIFall17MiniAODv2/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/PU2017_12Apr2018_new_pmx_94X_mc2017_realistic_v14_ext1-v1/120000/68D5B25A-D4EB-E811-9D8B-9CB65404ED04.root .
