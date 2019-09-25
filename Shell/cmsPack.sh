30953465bc1aab72ac6008bd6627f681c096debf
SangEun Lee <d4space@gmail.com>
2019-09-26 02:13:08 +0900

    up

--- Shell/cmsPack.sh
+++ Shell/cmsPack.sh
@@ -2,7 +2,8 @@
 export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
 source ${VO_CMS_SW_DIR}/cmsset_default.sh
 if [[ $HOSTNAME == *"ui"*".sdfarm.kr"* ]]; then
-  export SCRAM_ARCH=slc7_amd64_gcc700 # for 940 10_1_0
+  echo "Configuration for "$HOSTNAME
+  export SCRAM_ARCH=slc7_amd64_gcc700 # for 940 10_1_0 , 10_2_X
   source /cvmfs/cms.cern.ch/crab3/crab.sh
 fi
 #export SCRAM_ARCH=slc6_amd64_gcc530 # for 80X
---
+++ Shell/cmsswInst.sh
@@ -0,0 +1,4 @@
+echo "CMSSW installation"
+scram list |grep 10_2_
+scramv1 project -n CMSSW10215pch2 CMSSW_10_2_15_patch2
+
