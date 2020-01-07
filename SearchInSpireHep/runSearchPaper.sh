f9d55bb7fbfbee70e69c5de70b0eb4008467a641
SangEun Lee <d4space@gmail.com>
2020-01-07 16:19:54 +0900

    haha

--- SearchInSpireHep/runSearchPaper.sh
+++ SearchInSpireHep/runSearchPaper.sh
@@ -2,4 +2,4 @@
 
 #python3 searchInSpireHep.py -d --query "find author s. lee and tc p and d>=2018"
 #python3 searchInSpireHep.py -d --query "find author s. lee and cn cms and ac 1000+ and tc p and d 2018-01-01->2018-12-31"
-python3 searchInSpireHep.py -d --query "find author s. lee and cn cms and tc p and d 2018-01-01->2018-12-31"
+python3 searchInSpireHep.py -d --query "find author s. lee and cn cms and tc p and d 2019-03-01->2020-02-28"
